7259d59c557f76d42e6aadf0fd839812a4e10ee7
chakerox <chakerox@gmail.com>
2026-05-28 11:34:44 +0700

    mengcek apakah antrian penuh jika front 0 dan rear penu -1 atau front sama dengan rear +1 tampilkan antrian penuh

--- queue.cpp
+++ queue.cpp
@@ -22,3 +22,10 @@ private:
         cout << "Enter a number: ";
         cin >> num;
         cout << endl;
+
+          // cek apakah antrian penuh
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+        {
+            cout << "\nQueue overflow\n";
+            return;
+        }
